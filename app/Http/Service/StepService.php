0065abcb6031c03d40beaadaf3c8e251fceddfa1
sasamario <mametaku3@yahoo.co.jp>
2020-06-08 18:20:43 +0900

    Add：ログイン中のユーザーの歩数データ取得処理の追加 Add：ログイン中のユーザーの合計歩数取得処理の追加

--- app/Http/Service/StepService.php
+++ app/Http/Service/StepService.php
@@ -4,6 +4,7 @@ namespace App\Http\Service;
 
 use App\Http\Requests\StepRequest;
 use App\Step;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Auth;
 
 
@@ -19,5 +20,21 @@ class StepService
             'user_id' => Auth::id(),
             'date' => now(),
         ]);
+    }
+
+    /**
+     * @return LengthAwarePaginator
+     */
+    public function readStep(): LengthAwarePaginator
+    {
+        return Step::where('user_id', Auth::id())->orderBy('date', 'desc')->paginate(7);
+    }
+
+    /**
+     * @return int
+     */
+    public function readTotalStep(): int
+    {
+        return Step::where('user_id', Auth::id())->get()->sum('steps');
     }
 }
