44e02fa390a89d6882bd46240ddfc1c75255cbba
sasamario <mametaku3@yahoo.co.jp>
2020-06-08 17:58:33 +0900

    Add：StepServiceの追加 Add：歩数データ追加処理の実装

---
+++ app/Http/Service/StepService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Service;
+
+use App\Http\Requests\StepRequest;
+use App\Step;
+use Illuminate\Support\Facades\Auth;
+
+
+class StepService
+{
+    public function addStep(StepRequest $request): void
+    {
+        Step::create([
+            'steps' => $request->steps,
+            'user_id' => Auth::id(),
+            'date' => now(),
+        ]);
+    }
+}
