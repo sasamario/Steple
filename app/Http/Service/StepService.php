0ff2af9a30a3cd6cc624be0edb3147957fb853d0
sasamario <mametaku3@yahoo.co.jp>
2020-06-09 11:17:29 +0900

    Add：編集データ取得処理の追加 Add：歩数データの更新処理の追加

--- app/Http/Service/StepService.php
+++ app/Http/Service/StepService.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Service;
 
+use App\Http\Requests\EditStepRequest;
 use App\Http\Requests\StepRequest;
 use App\Step;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -51,5 +53,26 @@ class StepService
             ->orderBy('totalSteps', 'desc')
             ->limit(5)
             ->get();
+    }
+
+    /**
+     * @param Request $request
+     * @return Step
+     */
+    public function editStep(Request $request): Step
+    {
+        return Step::find($request->step_id);
+    }
+
+    /**
+     * @param EditStepRequest $request
+     */
+    public function updateStep(EditStepRequest $request): void
+    {
+        Step::where('step_id', $request->step_id)
+            ->update([
+                'date' => $request->date,
+                'steps' => $request->steps,
+            ]);
     }
 }
