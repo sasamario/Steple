5cea46e3e5dbb08131005a902b47847c25f22510
sasamario <mametaku3@yahoo.co.jp>
2020-06-09 11:18:35 +0900

    Add：編集ページに遷移するための処理の追加 Add：更新実行処理の追加

--- app/Http/Controllers/StepController.php
+++ app/Http/Controllers/StepController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\EditStepRequest;
 use App\Http\Requests\StepRequest;
 use App\Http\Service\StepService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StepController extends Controller
 {
@@ -39,5 +42,30 @@ class StepController extends Controller
         $rankingSteps = $this->stepService->readRankingStep();
 
         return view('home', compact('steps', 'totalSteps', 'rankingSteps'));
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\View\View
+     */
+    public function edit(Request $request)
+    {
+        $editStep = $this->stepService->editStep($request);
+        if ($editStep->user_id == Auth::id()) {
+            return view('step.edit', compact('editStep'));
+        } else {
+            return redirect()->route('home');
+        }
+    }
+
+    /**
+     * @param EditStepRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(EditStepRequest $request)
+    {
+        $this->stepService->updateStep($request);
+
+        return redirect()->route('home');
     }
 }
