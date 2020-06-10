8a6cfcac75878f7bbb86cab27fc38d23509672b4
sasamario <mametaku3@yahoo.co.jp>
2020-06-10 19:47:56 +0900

    Add：文字数デフォルトの設定 Add：本番環境でのhttpsの強制する処理の追加

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +27,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //Herokuで利用できるメモリの上限を超えてしまい機能しなくなってしまう為、文字数デフォルト255のところを191と指定
+        Schema::defaultStringLength(191);
+
+        // 本番環境(Heroku)でhttpsを強制する
+        if (App::environment('production')) {
+            URL::forceScheme('https');
+        }
+
     }
 }
